8805d2a75956d3a9abb797e050bc247acfdd9070
jichenjc <jichenjc@cn.ibm.com>
2019-04-17 02:54:10 +0000

    Fix ibmcloudclusterproviderspec_types
    remove specspec definition

--- pkg/apis/ibmcloud/v1alpha1/ibmcloudclusterproviderspec_types.go
+++ pkg/apis/ibmcloud/v1alpha1/ibmcloudclusterproviderspec_types.go
@@ -20,37 +20,17 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// IbmcloudClusterProviderSpecSpec defines the desired state of IbmcloudClusterProviderSpec
-type IbmcloudClusterProviderSpecSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-// IbmcloudClusterProviderSpecStatus defines the observed state of IbmcloudClusterProviderSpec
-type IbmcloudClusterProviderSpecStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// IbmcloudClusterProviderSpec is the Schema for the ibmcloudclusterproviderspecs API
+// IbmcloudClusterProviderSpec defines the desired state of IbmcloudClusterProvider
 // +k8s:openapi-gen=true
 type IbmcloudClusterProviderSpec struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   IbmcloudClusterProviderSpecSpec   `json:"spec,omitempty"`
-	Status IbmcloudClusterProviderSpecStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// IbmcloudClusterProviderSpecList contains a list of IbmcloudClusterProviderSpec
+// IbmcloudClusterProviderList contains a list of IbmcloudClusterProvider
 type IbmcloudClusterProviderSpecList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
