5385a5261609cea025d693c023bcf6df38912377
morvencao <morvencao@gmail.com>
2019-04-19 11:21:10 +0800

    fix if condition.

--- hack/verify-docfiles.sh
+++ hack/verify-docfiles.sh
@@ -22,7 +22,7 @@ CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
 [[ -z CHANGED_FILES ]] && exit 1
 
 for CHANGED_FILE in $CHANGED_FILES; do
-    if ! [[ $CHANGED_FILE =~ .md$]]; then
+    if ! [[ $CHANGED_FILE =~ .md$ ]]; then
         exit 1
     fi
 done
