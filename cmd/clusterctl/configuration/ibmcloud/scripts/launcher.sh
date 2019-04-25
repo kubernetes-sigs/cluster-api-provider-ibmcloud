1306b06f6f9930ce27c46e6d36a1512a5f58b770
xunpan <panpxpx@cn.ibm.com>
2019-04-24 20:18:41 -0400

    add launcher script. This script can be deleted if we know how to pass a script directly from user data in IBM cloud

---
+++ cmd/clusterctl/configuration/ibmcloud/scripts/launcher.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+USER_DATA_PATH=ibm_cloud_data
+USER_DATA_FILE=${USER_DATA_PATH}/openstack/latest/user_data
+mkdir ibm_cloud_data
+if [ $? -ne 0 ]; then
+    echo "Failed creating directory ${USER_DATA_PATH} under `pwd`."
+    exit -1
+fi
+
+mount /dev/xvdh1 ibm_cloud_data/
+if [ $? -ne 0 ]; then
+    echo "Failed mounting /dev/xvdh1 to `pwd`/${USER_DATA_PATH}."
+    exit -1
+fi
+
+if [ ! -f "${USER_DATA_FILE}" ];then
+    echo "Failed mounting /dev/xvdh1 to `pwd`/${USER_DATA_PATH}."
+    exit -1
+fi
+
+#base64 -d ibm_cloud_data/openstack/latest/user_data > ~/deployk8s.sh
+cp ibm_cloud_data/openstack/latest/user_data ~/deployk8s.sh
+bash -x ~/deployk8s.sh
