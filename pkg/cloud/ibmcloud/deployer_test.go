ddb26ce2bfebd170cabb18fdf5a2e19d8ae003cf
HaiHuiWang <wanghh2000@163.com>
2019-07-19 14:32:53 +0800

    add unit test for all functions

--- pkg/cloud/ibmcloud/deployer_test.go
+++ pkg/cloud/ibmcloud/deployer_test.go
@@ -20,7 +20,96 @@ import (
 	"os"
 	"strings"
 	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+	ibmcloudv1 "sigs.k8s.io/cluster-api-provider-ibmcloud/pkg/apis/ibmcloud/v1alpha1"
+	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
+	yaml "sigs.k8s.io/yaml"
 )
+
+func TestGetIP(t *testing.T) {
+	// exist IP case
+	testIP := "10.0.0.1"
+
+	cluster := &clusterv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testCluster",
+			Namespace: "testNamespace",
+		},
+	}
+
+	machine := &clusterv1.Machine{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testMachine",
+			Namespace: "testNamespace",
+			Annotations: map[string]string{
+				IBMCloudIPAnnotationKey: testIP,
+			},
+		},
+	}
+
+	deploy := NewDeploymentClient()
+	ip, err := deploy.GetIP(cluster, machine)
+	if err != nil || ip != testIP {
+		t.Errorf("Unable to get right machine IP %s", testIP)
+	}
+}
+
+func TestGetKubeConfig(t *testing.T) {
+	// test IP
+	testIP := "127.0.0.1"
+
+	cluster := &clusterv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testCluster",
+			Namespace: "testNamespace",
+		},
+	}
+	// ssh user name
+	tmpConfig := ibmcloudv1.IBMCloudMachineProviderSpec{
+		SshUserName: "testUser",
+	}
+
+	bytes, err := yaml.Marshal(tmpConfig)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	machine := &clusterv1.Machine{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testMachine",
+			Namespace: "testNamespace",
+			Annotations: map[string]string{
+				IBMCloudIPAnnotationKey: testIP,
+			},
+		},
+		Spec: clusterv1.MachineSpec{
+			ProviderSpec: clusterv1.ProviderSpec{
+				Value: &runtime.RawExtension{
+					Raw: bytes,
+				},
+			},
+		},
+	}
+
+	// test providerSpec
+	_, err = ibmcloudv1.MachineSpecFromProviderSpec(machine.Spec.ProviderSpec)
+	if err != nil {
+		t.Error(err)
+	}
+
+	deploy := NewDeploymentClient()
+	config, err := deploy.GetKubeConfig(cluster, machine)
+	if err != nil {
+		t.Error(err)
+	}
+	if config != "" {
+		t.Logf("Found kubeconfig: %s", config)
+	}
+
+}
 
 func TestGetSSHKeyFile(t *testing.T) {
 	// default case
