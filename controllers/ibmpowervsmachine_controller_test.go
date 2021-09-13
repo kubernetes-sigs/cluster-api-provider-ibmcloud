a5766245b5a6f5380d719380983933522649e469
Amulyam24 <30797554+Amulyam24@users.noreply.github.com>
2021-09-13 14:15:09 +0530

    unit tests-powervs (#339)

---
+++ controllers/ibmpowervsmachine_controller_test.go
@@ -0,0 +1,56 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kubernetes-sigs/cluster-api-provider-ibmcloud/api/v1alpha4"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+)
+
+var _ = Describe("IBMPowerVSMachineReconciler", func() {
+
+	Context("Reconcile an IBMPowerVSMachine", func() {
+		It("should not error or requeue the request", func() {
+			reconciler := &IBMPowerVSMachineReconciler{
+				Client: k8sClient,
+				Log:    klogr.New(),
+			}
+			By("Calling reconcile")
+			ctx := context.Background()
+			instance := &v1alpha4.IBMPowerVSMachine{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
+			result, err := reconciler.Reconcile(ctx, ctrl.Request{
+				NamespacedName: client.ObjectKey{
+					Namespace: instance.Namespace,
+					Name:      instance.Name,
+				},
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result.RequeueAfter).To(BeZero())
+			Expect(result.Requeue).To(BeFalse())
+		})
+	})
+})
