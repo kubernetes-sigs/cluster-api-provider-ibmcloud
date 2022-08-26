53c4f88942e36f9c763ffd08a19b8d00746e8de2
Prajyot Parab <prajyot.parab2@ibm.com>
2022-08-26 15:22:25 +0530

    Update golangci-lint to v1.49.0 (#823)
    Signed-off-by: Prajyot-Parab <prajyot.parab2@ibm.com>
    
    Signed-off-by: Prajyot-Parab <prajyot.parab2@ibm.com>

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// main is the main package for the Cluster API IBMCLOUD Provider.
 package main
 
 import (
