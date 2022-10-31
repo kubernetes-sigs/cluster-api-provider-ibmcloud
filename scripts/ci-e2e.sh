673928a9fb103e63befebd243aa09560c21ff198
Amulya Meka <amulmek1@in.ibm.com>
2022-10-31 13:10:43 +0530

    Use a fixed version of IBM Cloud CLI for e2e tests (#937)

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -18,10 +18,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# With the recent ibmcloud have seen panic and need this environment set to avoid this panic, for more information
-# refer the Notes section in https://github.com/IBM-Cloud/ibm-cloud-cli-release/releases/tag/v2.11.0.
-export LANG=en_US.UTF-8
-
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 cd "${REPO_ROOT}" || exit 1
 GOPATH_BIN="$(go env GOPATH)/bin/"
@@ -38,9 +34,13 @@ ARTIFACTS="${ARTIFACTS:-${PWD}/_artifacts}"
 mkdir -p "${ARTIFACTS}/logs/"
 
 ARCH=$(uname -m)
+OS=$(uname -s)
+IBMCLOUD_CLI_VERSION=${IBMCLOUD_CLI_VERSION:-"2.12.0"}
 PVSADM_VERSION=${PVSADM_VERSION:-"v0.1.7"}
 E2E_FLAVOR=${E2E_FLAVOR:-}
 REGION=${REGION:-"us-south"}
+
+[ "${ARCH}" == "x86_64" ] && ARCH="amd64"
 
 trap cleanup EXIT
 
@@ -49,17 +49,29 @@ cleanup(){
     [[ -z ${HEART_BEAT_PID:-} ]] || kill -9 "${HEART_BEAT_PID}" || true
 }
 
+# Installing binaries from github releases
 install_pvsadm(){
-    [ "${ARCH}" == "x86_64" ] && ARCH="amd64"
+    platform="$(echo ${OS} | tr '[:upper:]' '[:lower:]')-${ARCH}"
 
-    # Installing binaries from github releases
-    curl -fsL https://github.com/ppc64le-cloud/pvsadm/releases/download/${PVSADM_VERSION}/pvsadm-linux-${ARCH} -o pvsadm
+    curl -fsL https://github.com/ppc64le-cloud/pvsadm/releases/download/${PVSADM_VERSION}/pvsadm-${platform} -o pvsadm
     chmod +x ./pvsadm
 }
 
+install_ibmcloud_cli(){
+    if [ ${OS} == "Linux" ]; then
+        platform="linux-${ARCH}"
+    elif [ ${OS} == "Darwin" ]; then
+        platform="macos"
+    fi
+     
+    curl https://download.clis.cloud.ibm.com/ibm-cloud-cli/${IBMCLOUD_CLI_VERSION}/binaries/IBM_Cloud_CLI_${IBMCLOUD_CLI_VERSION}_${platform}.tgz -o IBM_Cloud_CLI_${IBMCLOUD_CLI_VERSION}_${platform}.tgz
+    tar -xf IBM_Cloud_CLI_${IBMCLOUD_CLI_VERSION}_${platform}.tgz
+    install IBM_Cloud_CLI/ibmcloud /usr/local/bin 
+
+}
+
 create_powervs_network_instance(){
-    # Install ibmcloud CLI tool
-    curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+    install_ibmcloud_cli
 
     # Login to IBM Cloud using the API Key
     ibmcloud login -a cloud.ibm.com -r ${REGION}
@@ -93,7 +105,7 @@ prerequisites_powervs(){
     # Assigning PowerVS variables
     export IBMPOWERVS_SSHKEY_NAME=${IBMPOWERVS_SSHKEY_NAME:-"powercloud-bot-key"}
     export IBMPOWERVS_IMAGE_NAME=${IBMPOWERVS_IMAGE_NAME:-"capibm-powervs-centos-streams8-1-24-2"}
-    export IBMPOWERVS_SERVICE_INSTANCE_ID=${BOSKOS_RESOURCE_ID:-"0f28d13a-6e33-4d86-b6d7-a9b46ff7659e"}
+    export IBMPOWERVS_SERVICE_INSTANCE_ID=${BOSKOS_RESOURCE_ID:-"d53da3bf-1f4a-42fa-9735-acf16b1a05cd"}
     export IBMPOWERVS_NETWORK_NAME="capi-net-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head --bytes 5)"
     # Setting controller loglevel to allow debug logs from the PowerVS client
     export LOGLEVEL=5
