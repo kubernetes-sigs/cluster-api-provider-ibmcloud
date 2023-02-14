66eddb321c961e3ddd12da2aef0f5f87d10559aa
Kishen V <110517346+kishen-v@users.noreply.github.com>
2023-02-14 18:39:31 +0530

    Support to create and delete ssh-keys in the PowerVS environment. (#1087)

---
+++ cmd/capibmadm/cmd/powervs/key/delete.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package key
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+
+	"github.com/IBM-Cloud/power-go-client/clients/instance"
+
+	"sigs.k8s.io/cluster-api/cmd/clusterctl/log"
+
+	"sigs.k8s.io/cluster-api-provider-ibmcloud/cmd/capibmadm/clients/iam"
+	"sigs.k8s.io/cluster-api-provider-ibmcloud/cmd/capibmadm/clients/powervs"
+	"sigs.k8s.io/cluster-api-provider-ibmcloud/cmd/capibmadm/options"
+	"sigs.k8s.io/cluster-api-provider-ibmcloud/cmd/capibmadm/utils"
+)
+
+// DeleteSSHKeyCommand - child command of 'key' to delete an SSH key.
+func DeleteSSHKeyCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delete",
+		Short: "Delete an SSH key in the PowerVS environment.",
+		Example: `
+# Delete an SSH key.
+export IBMCLOUD_API_KEY=<api-key>
+capibmadm powervs key delete --name <key-name> --service-instance-id <service-instance-id> --zone <zone>`,
+	}
+
+	var keyName string
+	cmd.Flags().StringVar(&keyName, "name", "", "The name of the SSH key.")
+	_ = cmd.MarkFlagRequired("name")
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		return deleteSSHKey(cmd.Context(), keyName)
+	}
+	return cmd
+}
+
+func deleteSSHKey(ctx context.Context, keyName string) error {
+	logger := log.Log
+	logger.Info("Deleting SSH key...")
+
+	auth := iam.GetIAMAuth()
+	accountID, err := utils.GetAccountID(ctx, auth)
+	if err != nil {
+		return err
+	}
+	sess, err := powervs.NewPISession(accountID, options.GlobalOptions.PowerVSZone, options.GlobalOptions.Debug)
+	if err != nil {
+		return err
+	}
+
+	keyClient := instance.NewIBMPIKeyClient(ctx, sess, options.GlobalOptions.ServiceInstanceID)
+	if err = keyClient.Delete(keyName); err != nil {
+		return err
+	}
+	logger.Info("Successfully deleted the SSH key.", "name", keyName)
+	return nil
+}
