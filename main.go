232e33b197c9db05008a2fdf8d47b8f282e2db16
Karthik K N <karthikkn1997@gmail.com>
2024-03-27 12:02:15 +0530

    Revert "Use uncached client (#1682)"
    This reverts commit aa44df2ca0691638e6484cebd5ec75af9868f8cb.

--- main.go
+++ main.go
@@ -34,7 +34,6 @@ import (
 
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	capiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
